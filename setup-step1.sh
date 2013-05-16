0b00f70138da82f23efe73528a5827e58abf4098
Neal Blackburn <neblackburn@gmail.com>
2013-05-15 18:24:48 -0700

    added my current two setup scrips

---
+++ setup-step1.sh
@@ -0,0 +1,44 @@
+# Stuff I want to install / update / upgrade as time goes on, can run multiple times
+# neal
+
+sudo apt-get update
+sudo apt-get upgrade
+
+sudo apt-get install \
+	apache2 \
+	mysql-client \
+	mysql-common \
+	mysql-server \
+	php5 \
+	libapache2-mod-php5 \
+	php5-cli \
+	php5-common \
+	php5-fpm \
+	php5-memcache \
+	php5-memcached \
+	php5-mysqlnd \
+	php5-xdebug \
+	php5-gd \
+	php5-imap \
+	php5-mcrypt \
+	php-pear \
+	php5-xmlrpc \
+	meld \
+	git \
+	vim \
+	vim-common \
+	screen \
+	openssh-server \
+	openssh-client \
+	lynx \
+	apache2-doc \
+	apache2-suexec \
+	php-pear \
+	uw-mailutils \
+	libipc-sharedcache-perl \
+	libmcrypt-dev \
+	mcrypt \
+	memcached
+
+sudo updatedb
+
---
+++ setup-step2.sh
@@ -0,0 +1,18 @@
+# Stuff I only want to run once... should only run once, because of the echo's into php.ini
+# neal
+
+sudo ufw enable
+sudo ufw allow 22
+sudo ufw allow 80
+sudo ufw allow 443
+
+sudo a2enmod rewrite
+
+XDEBUG=`locate xdebug.so`
+PHPINI=/etc/php5/apache2/php.ini
+
+echo '\n[Zend]' >> $PHPINI
+echo 'zend_extension=/us/lib/php5/20090626/xdebug.so' >> $PHPINI
+echo 'xdebug.remote_enable=1' >> $PHPINI
+echo 'xdebug.remote_host=localhost' >> $PHPINI
+echo 'xdebug.remote_port=9000' >> $PHPINI
